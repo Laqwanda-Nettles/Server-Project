d62ba98f690345ef2dbb9459e57569a41af9df43
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-11-08 16:03:11 -0600

    Add sidebar navigation

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,11 @@
+import Sidebar from "@/components/Sidebar";
+import Link from "next/link";
+
 export default function Home() {
   return (
-    <>
-      <h1>Server Project</h1>
-    </>
+    <div className="flex">
+      <Sidebar />
+      <h1 className="ml-[250px] p-8 text-3xl font-bold">Server Project</h1>
+    </div>
   );
 }
